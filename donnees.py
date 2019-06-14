0bfa6b8e18dbdb8dd8d674696420112678b86a69
Christian FOMEKONG <christian.fomekong@corpecca.africa>
2019-06-14 15:28:08 +0100

    [IMP]Mise à jour du fichier de données globales

--- donnees.py
+++ donnees.py
@@ -1,2 +1,17 @@
+liste_mots = [
+    "boutique",
+    "machine",
+    "papier",
+    "assiette",
+    "habit",
+    "poule",
+    "marmite",
+    "velo",
+    "voiture",
+    "permis"
+]
 
+coup_max = 8
+
+fichier_score = "scores"
     
